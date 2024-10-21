52ed5ba7fc1b8f36c73fa4cbffc58018523e9516
Kadu Oliveira <carloseduardo_al@hotmail.com>
2024-10-21 02:07:12 -0300

    lista funcionando

--- app.py
+++ app.py
@@ -2,8 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Caixa:
+    def __init__(self, id, data):
+        self.id = id
+        self.data = data
+        self.movimentacoes = []
+
+
 @app.route('/')
 def index():
-    return render_template('fechamento_caixa_simples.html')
 
-app.run()
+    caixa1 = Caixa(123, '2024-10-20')
+    caixa2 = Caixa(456, '2024-10-20')
+
+    lista = [caixa1, caixa2]
+
+    print(caixa1)
+    print(caixa2)
+    return render_template('lista.html', caixas=lista)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
